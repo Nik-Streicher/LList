33529758a617ae74137a6d41c4278b8d220fdc93
Nikolay <nikolay.sintsin@educanet.cz>
2020-11-09 09:43:18 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * @author Nikolay Sinitsin
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,8 @@
+/**
+ * @author Nikolay Sinitsin
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
